d0b3c14d2fd690be14400bac529c56fe0175f883
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-01 15:59:17 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+// math
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
