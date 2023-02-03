495822b10910f48c4fa1c18742320239a92c700f
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-03 09:55:22 -0800

    Added pokemon to js/updated css
    changed the boxes around (image/border)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+let pokemonList=[{name:"balbasure", type:['grass', 'poison'], height:'7'},
+                    {name: "eve", type:['water','speed'], height:'2'},
+                    {name:"charzard", type:['fire','flying'], height:'8'}];
+
+
+console.log(pokemonList);
+
+
 
