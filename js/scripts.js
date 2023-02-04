e1f8c2f175c065ff5c397ef59a4edbf8386dc5c9
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-04 11:03:11 -0800

    update: CSS/Index/JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-let pokemonList=[{name:"balbasure", type:['grass', 'poison'], height:'7'},
-                    {name: "eve", type:['water','speed'], height:'2'},
-                    {name:"charzard", type:['fire','flying'], height:'8'}];
 
 
-console.log(pokemonList);
+let pokemonList=[{name:'balbasure', type:['grass', 'poison'], height:'7'},  
+                 {name: 'eve', type:['water','speed'], height:'2'},
+                 {name:'charzard', type:['fire','flying'], height:'8'}];
 
 
 
+  document.write(pokemonList[0]);
+
+  
