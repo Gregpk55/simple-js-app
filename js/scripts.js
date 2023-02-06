8605a3a8025d8be65322a5c302a49559bc2419fc
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-05 20:42:13 -0800

    JavaScript update
    including outputs

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,21 @@ let pokemonList=[{name:'balbasure', type:['grass', 'poison'], height:'7'},
                  {name:'charzard', type:['fire','flying'], height:'8'}];
 
 
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height <8 && pokemonList[i].height >4){
+    document.write('<p>',pokemonList[i].name + ' is a medium sized pokemon.');
 
-  document.write(pokemonList[0]);
+}else if(pokemonList[i].height <5){
+    document.write('<p>',pokemonList[i].name +  ' is a small pokemon.');
+
+}else {
+    document.write('<p>',pokemonList[i].name + ' is a big pokemon!');
+} 
+document.write (pokemonList[0],(height));
+  
+
+}
+
 
   
+
