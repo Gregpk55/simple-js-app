15ff998b5d14b73dbc850f764a19a350e6d782dc
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-07 11:21:09 -0800

    updated formatting of all docs

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,16 @@
+let pokemonList = [
+  { name: "balbasure", type: ["grass", "poison"], height: "7" },
+  { name: "eve", type: ["water", "speed"], height: "2" },
+  { name: "charzard", type: ["fire", "flying"], height: "8" },
+];
 
-
-let pokemonList=[{name:'balbasure', type:['grass', 'poison'], height:'7'},  
-                 {name: 'eve', type:['water','speed'], height:'2'},
-                 {name:'charzard', type:['fire','flying'], height:'8'}];
-
-
-for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <8 && pokemonList[i].height >4){
-    document.write('<p>',pokemonList[i].name + ' is a medium sized pokemon.');
-
-}else if(pokemonList[i].height <5){
-    document.write('<p>',pokemonList[i].name +  ' is a small pokemon.');
-
-}else {
-    document.write('<p>',pokemonList[i].name + ' is a big pokemon!');
-} 
-document.write (pokemonList[0],(height));
-  
-
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 8 && pokemonList[i].height > 4) {
+    document.write("<p>", pokemonList[i].name + " is a medium sized pokemon.");
+  } else if (pokemonList[i].height < 5) {
+    document.write("<p>", pokemonList[i].name + " is a small pokemon.");
+  } else {
+    document.write("<p>", pokemonList[i].name + " is a big pokemon!");
+  }
+  document.write(pokemonList[i].height);
 }
-
-
-  
-
