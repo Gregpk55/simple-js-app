f69bf7dddcdd138aaf945a3c2f0bcc48191835db
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-02-09 20:12:59 -0800

    1.5 add functions/push

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
-let pokemonList = [
-  { name: "balbasure", type: ["grass", "poison"], height: "7" },
-  { name: "eve", type: ["water", "speed"], height: "2" },
-  { name: "charzard", type: ["fire", "flying"], height: "8" },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Balbasure", type: ["grass", "poison"], height: "71.1 cm" },
+    { name: "Eve", type: ["water", "speed"], height: "30.5 cm" },
+    { name: "Charzard", type: ["fire", "flying"], height: "1.7 m" },
+  ];
 
-pokemonList.forEach(function (pokemon) {
-  document.write(pokemon.name + pokemon.height + "<br>");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+pokemonRepository.add({ name: "Squirtle", type: "water", height: "50.8 cm" });
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(pokemon.name + ", " + pokemon.height + "<br>");
 });
